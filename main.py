6e163ac2fe6c0284bda0d4a01255d872282eb1f7
EriveltonJr <franciscoerivelton12@gmail.com>
2024-08-29 16:28:41 -0300

    Tempo de execução corrigido

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import time
+import timeit
 import tracemalloc
 from tab import BTree
 
 def medir_tempo(func):
     """Decorator para medir o tempo de execução de uma função em milissegundos."""
     def wrapper(*args, **kwargs):
-        inicio = time.time()
-        resultado = func(*args, **kwargs)
-        fim = time.time()
-        tempo_execucao = (fim - inicio) * 1000  # Convertendo para milissegundos
-        return resultado, tempo_execucao
+        tempo_execucao = timeit.timeit(lambda: func(*args, **kwargs), number=1) * 1000  # Convertendo para milissegundos
+        print(f"Tempo de execução da função '{func.__name__}': {tempo_execucao:.6f} ms")
+        return tempo_execucao
     return wrapper
 
 @medir_tempo
@@ -64,7 +62,7 @@ def main():
                 id = int(input("Digite o ID: "))
                 nome = input("Digite o nome: ")
                 idade = int(input("Digite a idade: "))
-                _, tempo_execucao = inserir_registro(btree, id, nome, idade)
+                tempo_execucao = inserir_registro(btree, id, nome, idade)
                 tempos_execucao["inserir"].append(tempo_execucao)
                 print(f"Registro {id} inserido com sucesso. Tempo de execução: {tempo_execucao:.6f} ms")
                 continuar = input("Deseja inserir outro registro? (s/n): ")
@@ -74,10 +72,10 @@ def main():
         elif escolha == '2':
             while True:
                 id = int(input("Digite o ID a ser buscado: "))
-                resultado, tempo_execucao = buscar_registro(btree, id)
+                tempo_execucao = buscar_registro(btree, id)
                 tempos_execucao["buscar"].append(tempo_execucao)
-                if resultado:
-                    print(f"Registro encontrado: {resultado}. Tempo de execução: {tempo_execucao:.6f} ms")
+                if tempo_execucao:
+                    print(f"Registro com ID {id} encontrado. Tempo de execução: {tempo_execucao:.6f} ms")
                 else:
                     print(f"Registro com ID {id} não encontrado. Tempo de execução: {tempo_execucao:.6f} ms")
                 continuar = input("Deseja buscar outro registro? (s/n): ")
@@ -89,12 +87,9 @@ def main():
                 id = int(input("Digite o ID do registro a ser atualizado: "))
                 nome = input("Digite o novo nome: ")
                 idade = int(input("Digite a nova idade: "))
-                sucesso, tempo_execucao = atualizar_registro(btree, id, nome, idade)
+                tempo_execucao = atualizar_registro(btree, id, nome, idade)
                 tempos_execucao["atualizar"].append(tempo_execucao)
-                if sucesso:
-                    print(f"Registro {id} atualizado com sucesso. Tempo de execução: {tempo_execucao:.6f} ms")
-                else:
-                    print(f"Erro ao atualizar o registro com ID {id}. Tempo de execução: {tempo_execucao:.6f} ms")
+                print(f"Registro {id} atualizado com sucesso. Tempo de execução: {tempo_execucao:.6f} ms")
                 continuar = input("Deseja atualizar outro registro? (s/n): ")
                 if continuar.lower() != 's':
                     break
@@ -102,7 +97,7 @@ def main():
         elif escolha == '4':
             while True:
                 id = int(input("Digite o ID do registro a ser removido: "))
-                _, tempo_execucao = remover_registro(btree, id)
+                tempo_execucao = remover_registro(btree, id)
                 tempos_execucao["remover"].append(tempo_execucao)
                 print(f"Registro {id} removido com sucesso. Tempo de execução: {tempo_execucao:.6f} ms")
                 continuar = input("Deseja remover outro registro? (s/n): ")
